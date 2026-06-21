e6ca8df9fac929ebe7be44c041d6b08a8a785e99
Jakob1312161 <petermeeuwsen@posteo.net>
2026-06-21 19:29:19 +0200

    Movement Script
    Gibt noch bugs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private CharacterController controller;
+
     public float speed = 12f;
     public float gravity = -9.81f * 2;
     public float jumpHeight = 3f;
@@ -10,6 +12,9 @@ public class PlayerMovement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
+    // Set to true in the Inspector if horizontal/vertical input should be swapped
+    public bool swapControls = false;
+
     Vector3 velocity;
     bool isGrounded;
 
@@ -17,11 +22,50 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        
+        //Ground check
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        //Reseting the default velocity
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Getting the input from the player
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Moving the player (allow swapping if controls are reversed in the Input settings)
+        Vector3 move;
+        if (swapControls)
+        {
+            move = transform.right * vertical + transform.forward * horizontal;
+        }
+        else
+        {
+            move = transform.right * horizontal + transform.forward * vertical;
+        }
+
+        //Actually moving the player
+        controller.Move(move * speed * Time.deltaTime);
+
+        //Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            //Actully jumping
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //Falling down
+        velocity.y += gravity * Time.deltaTime;
+
+        //Executing the jump
+        controller.Move(velocity * Time.deltaTime);
+
+        lastPosition = gameObject.transform.position;
     }
 }
