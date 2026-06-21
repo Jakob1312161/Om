be7c814fa645f52c6495267f4d35b6d34d559647
Jakob1312161 <petermeeuwsen@posteo.net>
2026-06-21 18:35:01 +0200

    MoveScript

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 12f;
+    public float gravity = -9.81f * 2;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    private Vector3 lastPosition = new Vector3(0f, 0f, 0f);
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
